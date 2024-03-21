784df1b4016a2b63edbedcb7b9ef29b2760a679b
ochiengsulwe <kodongochieng@gmail.com>
2024-03-21 20:00:05 +0300

    Deployment configurations

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,7 +22,7 @@ if [ -L "/data/web_static/current" ]; then
     sudo rm /data/web_static/current
 fi
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/currrent
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/^\s*server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
