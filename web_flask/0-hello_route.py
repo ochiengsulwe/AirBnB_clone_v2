5dfe53f99a3ab4a531f358736883d902a7e81a57
ochiengsulwe <kodongochieng@gmail.com>
2024-03-27 11:36:29 +0300

    Setting up Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Starts a Flask we application."""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Routing to root, with strict_slashes to ensure the URL works when
+        when it ends both with or without the `/`.
+    """
+    return "Hell0 HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=500)
