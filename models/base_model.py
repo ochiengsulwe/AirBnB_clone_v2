e6deec6240bb4920027d104d5309c288c33d407f
ochiengsulwe <kodongochieng@gmail.com>
2024-04-10 15:42:55 +0300

    Making things neat and simple

--- models/base_model.py
+++ models/base_model.py
@@ -30,8 +30,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        cls = f"{self.__class__.__name__}"
+        return f"[{cls}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
